a144c99df6295305701c75b8a71be2a224b4f75d
Diana Zavala <diana_zavalaa@hotmail.com>
2024-01-18 10:20:02 -0600

    Agregando componentes reutilizables part1

---
+++ src/components/Image.vue
@@ -0,0 +1,16 @@
+<script lang="ts">
+
+export default{
+    props: {
+        url: String, 
+        id: String,
+        title: String
+    }
+        
+};
+
+</script>
+
+<template>
+    <img  class="max-h-40 rounded-lg"  :src="url"  :alt="title" />
+</template>
